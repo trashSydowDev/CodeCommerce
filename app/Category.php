a52688d2d3365dfdcb05764bad4b2b44ccc78d6f
Candido Souza <candido_souzza@hotmail.com>
2015-06-25 15:58:24 -0300

    Finalizando relacionamento entre tabelas categories e products

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'description'];
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany('CodeCommerce\Products');
+    }
+
+
 }
